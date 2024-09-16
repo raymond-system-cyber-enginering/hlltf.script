1d493389d1dee5e9658fbd8cf90026dda2eab3e8
raymond-system-cyber-enginering <winxen.10388@ski.sch.id>
2024-09-16 12:28:20 +0700

    <script> beggod.sh
    beggod is a begtagood company

---
+++ beggod.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+clear
+echo
+sleep 1
+figlet "begtagood"
+echo
+echo "version : 3.0.0"
+echo
+echo "__________________________________________"
+echo "support me !!"
+echo "tiktok : mr anonmoux"
+echo "instagram : yshrillboom_03"
+echo "contact me : 6285141705599"
+echo "__________________________________________"
+echo
+echo
+echo
+echo "please choose number : "
+echo
+echo "1.join_begtagood-wa-channel"
+echo
+echo "2.join_begtagood-wa-group"
+echo
+echo "3.vvip"
+echo
+echo "4.virtex"
+echo
+echo "5.exit"
+echo
+echo "6.about"
+echo
+echo "7.my ff id"
+echo
+read -p "choose number : " br
+
+if [ $br = 1 ] || [ $br = 1 ]
+then
+clear
+sleep 1
+echo
+figlet "wa channel"
+echo
+echo
+echo "channel links :"
+echo
+echo "copy this link to chrome or browser!!"
+echo
+echo "https://whatsapp.com/channel/0029VagKRi17Noa6f4DnkV0G"
+echo
+echo "thanks for join!!"
+echo
+echo  "automaticaly exit"
+echo "••••••••••••••"|pv -qL 10
+sleep 1
+exit
+fi
+
+if [ $br = 2 ] || [ $br = 2 ]
+then
+clear
+sleep 2
+echo
+figlet "group"
+echo
+echo "group links :"
+echo
+echo "copy this link to chrome or browser!!"
+echo
+echo "https://chat.whatsapp.com/CPv6kyeLmmhIxdukt0whC5"
+echo
+echo "thanks for join!!"
+echo
+echo "automaticaly exit"
+echo "•••••••••••••"|pv -qL 10
+sleep 2
+exit
+fi
+
+if [ $br = 3 ] || [ $br = 3 ]
+then
+clear
+sleep 3
+echo
+figlet "vvip"
+echo
+echo "please enter pass for vvip"
+echo
+read -p "password : " b
+fi
+
+if [ $b = winxbv ] || [ $b = winxbv ]
+then
+sleep 3
+echo "vvip unlocked!!!"
+echo
+echo " lets go vvip register in wa : 6285141705599"
+echo
+echo "automaticaly exit"
+echo "•••••••••••••"|pv -qL 10
+exit
+fi
+
+if [ $br = 4 ] || [ $br = 4 ]
+then
+clear
+sleep 4
+echo
+figlet "virtex"
+sleep 4
+echo
+echo "virtex"
+echo
+echo "copy the link to browser or chrome to get virtex!!"
+echo
+echo "https://www.mediafire.com/file/fhc2uslo44odx6t/virtex-crazy-bug.txt/file"
+echo
+echo "want more virtex? buy in my wa store!!"
+echo
+echo "my wa : 6285141705599"
+echo
+echo "automaticaly exit..."
+echo "•••••••••••••"|pv -qL 10
+exit
+fi
+
+if [ $br = 5 ] || [ $br = 5 ]
+then
+clear
+sleep 5
+echo
+figlet "exit"
+sleep 5
+clear
+echo
+figlet "bye!!"
+sleep 5
+exit
+fi
+
+if [ $br = 6 ] || [ $br = 6 ]
+then
+clear
+sleep 6
+echo
+figlet "about"|pv -qL 35
+sleep 6
+echo "developer : raymond l.lostan"|pv -qL 10
+echo "sc ke : 1"|pv -qL 10
+echo "============================"|pv -qL 10
+echo
+echo "automaticaly exit"
+echo "•••••••••••••"|pv -qL 10
+exit
+fi
+
+if [ $br = 7 ] || [ $br = 7 ]
+then
+clear
+sleep 7
+echo
+figlet "ff id"
+echo
+echo "my ff id : "
+echo
+echo "9459758502"
+echo
+echo "automaticaly exit..."
+echo "•••••••••••••"|pv -qL 10
+exit
+fi
